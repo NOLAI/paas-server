5531ea7bad621fc310b07fa4202cd313f69969f4
Julian van der Horst <julianvanderhorst@pm.me>
2024-10-08 12:01:33 +0200

    Update libpep

--- src/application.rs
+++ src/application.rs
@@ -5,9 +5,7 @@ use actix_web::web::{Bytes, Data};
 use libpep::arithmetic::ScalarTraits;
 use libpep::distributed::PEPSystem;
 use libpep::elgamal::{ElGamal};
-use libpep::high_level::EncryptionContext;
-use libpep::primitives::rsk_from_to;
-use libpep::utils::{make_decryption_factor, make_pseudonymisation_factor};
+use libpep::high_level::{EncryptedPseudonym, EncryptionContext, PseudonymizationContext};
 use serde::{Deserialize, Serialize};
 use crate::auth_middleware::AuthenticationInfo;
 use crate::domain_middleware::DomainInfo;
@@ -21,10 +19,10 @@ pub struct EncryptedPseudonymResponse {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct PseudonymizationRequest {
     encrypted_pseudonym: String,
-    pseudonym_context_from: String,
-    pseudonym_context_to: String,
-    enc_context: String,
-    dec_context: String,
+    pseudonym_context_from: PseudonymizationContext,
+    pseudonym_context_to: PseudonymizationContext,
+    enc_context: EncryptionContext,
+    dec_context: EncryptionContext,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -53,10 +51,10 @@ pub async fn status() -> impl Responder {
          })
 }
 
-fn hex_to_blindingfactor(s: &str) -> libpep::distributed::BlindingFactor {
-    let scalar = libpep::arithmetic::ScalarNonZero::decode_from_hex(s).unwrap();
-    libpep::distributed::BlindingFactor(scalar)
-}
+// fn hex_to_blindingfactor(s: &str) -> libpep::distributed::BlindingFactor {
+//     let scalar = libpep::arithmetic::ScalarNonZero::decode_from_hex(s).unwrap();
+//     libpep::distributed::BlindingFactor(scalar)
+// }
 
 pub async fn random() -> impl Responder {
     let random = libpep::arithmetic::GroupElement::random(&mut rand::thread_rng());
@@ -74,33 +72,20 @@ pub async fn random() -> impl Responder {
     })
 }
 
-fn rsk(msg_in: ElGamal, pseudonym_context_from: String, pseudonym_context_to: String, enc_context: String, dec_context: String) -> ElGamal {
-    let v_from = make_pseudonymisation_factor(&"secret".to_string(), &pseudonym_context_from);
-    let v_to = make_pseudonymisation_factor(&"secret".to_string(), &pseudonym_context_to);
-    let k_from = make_decryption_factor(&"secret".to_string(), &enc_context);
-    let k_to = make_decryption_factor(&"secret".to_string(), &dec_context);
 
-    rsk_from_to(&msg_in, &v_from, &v_to, &k_from, &k_to)
-}
+fn has_access_to_context(from: Arc<Vec<String>>, to: Arc<Vec<String>>, enc_context: EncryptionContext, dec_context: EncryptionContext, user_sessions: Vec<String>) -> bool {
+    // Access control alleen bij de prefix en niet postfix. Voor nu postfix loggen.
+    // dec_context moet gelijk zijn aan jou sessie. 
 
-
-fn has_access_to_context(from: Arc<Vec<String>>, to: Arc<Vec<String>>, enc_context: String, dec_context: String, user_sessions: Vec<String>) -> bool {
     user_sessions.contains(&dec_context) && from.contains(&enc_context) && to.contains(&dec_context)
 }
 
-pub async fn pseudonymize(req: HttpRequest, body: Bytes,  redis: Data<RedisConnector>) -> impl Responder {
-
-    // Access control alleen bij de prefix en niet postfix. Voor nu postfix loggen.
-    // dec_context moet gelijk zijn aan jou sessie. 
-        
+pub async fn pseudonymize(req: HttpRequest, body: Bytes,  redis: Data<RedisConnector>,  pep_system: Data<PEPSystem>) -> impl Responder {
+    
     let auth = req.extensions().get::<AuthenticationInfo>().unwrap().clone();
     let domain_info = req.extensions().get::<DomainInfo>().unwrap().clone();
     let item = serde_json::from_slice::<PseudonymizationRequest>(&body);
-    // 
-    // println!("{:?}", item); // <- print request body
-    // println!("{:?}", auth); // <- print authentication info
-    // println!("{:?}", domain_info); // <- print domain info
-    // 
+
     let request = item.unwrap();
 
     let mut redis_connector = redis.get_ref().clone();
@@ -114,9 +99,11 @@ pub async fn pseudonymize(req: HttpRequest, body: Bytes,  redis: Data<RedisConne
     if msg_in.is_none() {
         return HttpResponse::BadRequest().body("Invalid input");
     }
-    let msg_in = msg_in.unwrap();
-
-    let msg_out = rsk(msg_in, request.pseudonym_context_from, request.pseudonym_context_to, request.enc_context, request.dec_context);
+    let msg_in = EncryptedPseudonym::new(msg_in.unwrap());
+    let msg_out = pep_system.pseudonymize(&msg_in, 
+                                          &pep_system.pseudonymization_info(
+                                              &request.pseudonym_context_from, &request.pseudonym_context_to, 
+                                              &request.enc_context, &request.dec_context));
 
     HttpResponse::Ok().json(EncryptedPseudonymResponse {
         encrypted_pseudonym: msg_out.encode_to_base64(),
