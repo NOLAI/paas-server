cb3e219ec9f135a6f4684d4516e6b25c26581b1b
Job Doesburg <job.doesburg@gmail.com>
2024-12-19 10:14:50 +0100

    fix: Update import path for access rules in test file

--- tests/test_access_rules.rs
+++ tests/test_access_rules.rs
@@ -1,4 +1,4 @@
-use PaaS_server::access_rules::{AccessRules, AuthenticatedUser, Permission};
+use crate::access_rules::{AccessRules, AuthenticatedUser, Permission};
 use libpep::high_level::contexts::PseudonymizationContext;
 use chrono::Utc;
 use std::collections::HashSet;
