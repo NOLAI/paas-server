168f1e35ad4c4431dc1ff6ac6a0ad51755d06351
Julian van der Horst <JA.vanderHorst@student.science.ru.nl>
2025-02-05 15:17:49 +0100

    Added FromStr to AuthTokens

--- client/rs/src/auth.rs
+++ client/rs/src/auth.rs
@@ -2,6 +2,7 @@ use crate::transcryptor_client::AuthToken;
 use paas_common::status::SystemId;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::str::FromStr;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct AuthTokens(pub HashMap<SystemId, AuthToken>);
@@ -9,5 +10,14 @@ pub struct AuthTokens(pub HashMap<SystemId, AuthToken>);
 impl AuthTokens {
     pub fn get(&self, system_id: &SystemId) -> Option<&AuthToken> {
         self.0.get(system_id)
+    }
+}
+
+impl FromStr for AuthTokens {
+    type Err = serde_json::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let map: HashMap<SystemId, AuthToken> = serde_json::from_str(s)?;
+        Ok(Self(map))
     }
 }
